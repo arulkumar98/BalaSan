a627f8d5eeb078dde536dff6ce4c88a190ce63ab
Arul Kumar <arulkumar202@gmail.com>
2022-09-21 17:43:07 +0530

    Initial commit

---
+++ src/Price.java
@@ -0,0 +1,7 @@
+package inter;
+//All the method in INTERFACE are abstract
+
+public interface Price {
+    public void rate();
+
+}
---
+++ src/jaguar.java
@@ -0,0 +1,9 @@
+package inter;
+
+public class jaguar implements Price {
+
+    public void rate(){
+        System.out.println("1.5 Million $");
+    }
+
+}
